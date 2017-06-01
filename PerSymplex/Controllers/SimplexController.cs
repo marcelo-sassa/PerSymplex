26ec94562bf13c2a584710b063e9b16265fd00cd
Marcelo Sassá <marcelosassa@univem.edu.br>
2017-06-01 15:29:04 -0300

    Migração e ajustes de Views e Funções + Implemetação do Algoritmo Simplex

--- PerSymplex/Controllers/SimplexController.cs
+++ PerSymplex/Controllers/SimplexController.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using PerSymplex.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using PerSymplex.Models;
 
 namespace PerSymplex.Controllers
 {
-    public class HomeController : Controller
+    public class SimplexController : Controller
     {
+        // GET: Simplex
         [HttpGet]
         public ActionResult Index()
         {
@@ -109,14 +110,113 @@ namespace PerSymplex.Controllers
                 }
             }
 
+            if (MatrixA != null)
+            {
+                HeaderSimplex ModelTopZera = new HeaderSimplex();
+                ModelTopZera = Model;
+                while (!CondicaoDeParada(ModelTopZera.Matriz))
+                {
+                    ModelTopZera.Matriz = SolveSimplex(Model);
+                }
+            }
+
             if(MatrixA == null)
             {
                 return View(Model);
             }
             else
             {
-                return View("MatrizPreenchida",Model);
+                return View("TabelaInicial",Model);
             }
+        }
+
+        public string[,] SolveSimplex(HeaderSimplex Model)
+        {
+            int PCol, PLin;
+            string[,] NovaTabela = new string[Model.Matriz.GetLength(0), Model.Matriz.GetLength(1)];
+            
+            PCol = findPCol(Model.Matriz);
+            PLin = findPLin(Model.Matriz, PCol);
+            //basis[PLin] = PCol;
+
+            for (int i = 0; i < Model.Matriz.GetLength(0); i++)
+                NovaTabela[i, 0] = Model.Matriz[i, 0];
+
+            for (int j = 0; j < Model.Matriz.GetLength(1); j++)
+                NovaTabela[0, j] = Model.Matriz[0,j];
+
+            NovaTabela[PLin, 0] = Model.Matriz[0, PCol];
+
+            for (int j = 1; j < Model.Matriz.GetLength(1); j++)
+                NovaTabela[PLin, j] = (decimal.Parse(Model.Matriz[PLin, j]) / decimal.Parse(Model.Matriz[PLin, PCol])).ToString();
+
+            for (int i = 1; i < Model.Matriz.GetLength(0); i++)
+            {
+                if (i == PLin)
+                    continue;
+
+                for (int j = 1; j < Model.Matriz.GetLength(1); j++)
+                    NovaTabela[i, j] = (decimal.Parse(NovaTabela[PLin, j]) * (decimal.Parse(Model.Matriz[i, PCol]) * (-1)) + decimal.Parse(Model.Matriz[i,j])).ToString();
+                //NovaTabela[i, j] = Model.Matriz[i, j] - Model.Matriz[i, PCol] * NovaTabela[PLin, j];
+            }
+            //table = NovaTabela;
+
+            // Count a result found by the X
+            //for (int i = 0; i < result.Length; i++)
+            //{
+            //    int k = basis.IndexOf(i + 1);
+            //    if (k != -1)
+            //        result[i] = table[k, 0];
+            //    else
+            //        result[i] = 0;
+            //}
+            return NovaTabela;
+        }
+
+        public bool CondicaoDeParada(string[,] Tabela)
+        {
+            for (int i = (Tabela.GetLength(0) - 1); i < Tabela.GetLength(0); i++)
+            {
+                for (int j = 1; j < Tabela.GetLength(1); j++)
+                {
+                    if(decimal.Parse(Tabela[i,j]) < 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private int findPCol(string[,] Tabela)
+        {
+            int PCol = 1;
+
+            for (int j = 2; j < (Tabela.GetLength(1) - 1); j++)
+                if (decimal.Parse(Tabela[(Tabela.GetLength(0) - 1), j]) < decimal.Parse(Tabela[(Tabela.GetLength(0) - 1), PCol]))
+                    PCol = j;
+
+            return PCol;
+        }
+
+        private int findPLin(string[,] Tabela,int PCol)
+        {
+            int PLin = 1;
+
+            for (int i = 1; i < (Tabela.GetLength(0) - 1); i++)
+                if (decimal.Parse(Tabela[i, PCol]) > 0)
+                {
+                    PLin = i;
+                    break;
+                }
+
+            for (int i = PLin + 1; i < (Tabela.GetLength(0) - 1) - 1; i++)
+                if ((decimal.Parse(Tabela[i, PCol]) > 0))
+                    if(((decimal.Parse(Tabela[i, (Tabela.GetLength(1) - 1)]) / decimal.Parse(Tabela[i, PCol])) < (decimal.Parse(Tabela[PLin, (Tabela.GetLength(1) - 1)]) / decimal.Parse(Tabela[PLin, PCol]))))
+                        PLin = i;
+
+            return PLin;
         }
 
         [HttpGet]
