c45a4d537619460445c14f1148daa025e437ec3e
Marcelo Sassá <marcelosassa@univem.edu.br>
2017-06-02 16:02:24 -0300

    Tratativa de soluções ilimitadas

--- PerSymplex/Controllers/SimplexController.cs
+++ PerSymplex/Controllers/SimplexController.cs
@@ -26,118 +26,134 @@ namespace PerSymplex.Controllers
 
             List<HeaderSimplex> ListaTabelas = new List<HeaderSimplex>();
 
-            if (MatrixA != null)
+            try
             {
-                if (Model.Matriz == null)
+                if (MatrixA != null)
                 {
-                    Model.Matriz = new string[(Model.NRest + 2), (Model.NRest + Model.NVar + 2)];
-                }
-                for (int i = 0; i < Model.Matriz.GetLength(0); i++)
-                {
-                    for (int j = 0; j < Model.Matriz.GetLength(1); j++)
+                    if (Model.Matriz == null)
                     {
-                        if(i == 0)
+                        Model.Matriz = new string[(Model.NRest + 2), (Model.NRest + Model.NVar + 2)];
+                    }
+                    for (int i = 0; i < Model.Matriz.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < Model.Matriz.GetLength(1); j++)
                         {
-                            if(j == 0)
+                            if (i == 0)
                             {
-                                Model.Matriz[i, j] = "Base";
-                            }
-                            else if(j > 0 & j <= Model.NVar)
-                            {
-                                Model.Matriz[i, j] = "X" + j;
-                            }
-                            else if(j > Model.NVar & j < (Model.Matriz.GetLength(1) - 1))
-                            {
-                                Model.Matriz[i, j] = "F" + (j-Model.NVar);
-                            }
-                            else
-                            {
-                                Model.Matriz[i, j] = "b";
-                            }
-                        }
-                        else if(j == 0)
-                        {
-                            if(i <= Model.NRest)
-                            {
-                                Model.Matriz[i, j] = "F" + i;
-                            }
-                            else
-                            {
-                                Model.Matriz[i, j] = "Z";
-                            }
-                        }
-                        else
-                        {
-                            if(i <= Model.NRest)
-                            {
-                                if(j <= Model.NVar)
+                                if (j == 0)
                                 {
-                                    string valor = MatrixA[(i - 1), (j - 1)];
-                                    Model.Matriz[i, j] = (!string.IsNullOrEmpty(valor) ? valor : "0");
+                                    Model.Matriz[i, j] = "Base";
                                 }
-                                else if(j > Model.NVar & j < (Model.Matriz.GetLength(1) - 1))
+                                else if (j > 0 & j <= Model.NVar)
                                 {
-                                    if(i == (j-Model.NVar))
+                                    Model.Matriz[i, j] = "X" + j;
+                                }
+                                else if (j > Model.NVar & j < (Model.Matriz.GetLength(1) - 1))
+                                {
+                                    Model.Matriz[i, j] = "F" + (j - Model.NVar);
+                                }
+                                else
+                                {
+                                    Model.Matriz[i, j] = "b";
+                                }
+                            }
+                            else if (j == 0)
+                            {
+                                if (i <= Model.NRest)
+                                {
+                                    Model.Matriz[i, j] = "F" + i;
+                                }
+                                else
+                                {
+                                    Model.Matriz[i, j] = "Z";
+                                }
+                            }
+                            else
+                            {
+                                if (i <= Model.NRest)
+                                {
+                                    if (j <= Model.NVar)
                                     {
-                                        Model.Matriz[i, j] = "1";
+                                        string valor = MatrixA[(i - 1), (j - 1)];
+                                        Model.Matriz[i, j] = (!string.IsNullOrEmpty(valor) ? valor : "0");
+                                    }
+                                    else if (j > Model.NVar & j < (Model.Matriz.GetLength(1) - 1))
+                                    {
+                                        if (i == (j - Model.NVar))
+                                        {
+                                            Model.Matriz[i, j] = "1";
+                                        }
+                                        else
+                                        {
+                                            Model.Matriz[i, j] = "0";
+                                        }
+                                    }
+                                    else
+                                    {
+                                        string valor = MatrixA[(i - 1), (MatrixA.GetLength(1) - 1)];
+                                        Model.Matriz[i, j] = (!string.IsNullOrEmpty(valor) ? valor : "0");
+                                    }
+                                }
+                                else
+                                {
+                                    if (j <= Model.NVar)
+                                    {
+                                        string valor = (!string.IsNullOrEmpty(Model.FO[j - 1]) ? Model.FO[j - 1] : "0");
+
+                                        decimal valorConv = decimal.Parse(valor);
+                                        valorConv = valorConv * (-1);
+                                        Model.Matriz[i, j] = valorConv.ToString();
                                     }
                                     else
                                     {
                                         Model.Matriz[i, j] = "0";
                                     }
                                 }
-                                else
-                                {
-                                    string valor = MatrixA[(i - 1), (MatrixA.GetLength(1) - 1)];
-                                    Model.Matriz[i, j] = (!string.IsNullOrEmpty(valor) ? valor : "0");
-                                }
-                            }
-                            else
-                            {
-                                if (j <= Model.NVar)
-                                {
-                                    string valor = (!string.IsNullOrEmpty(Model.FO[j - 1]) ? Model.FO[j - 1] : "0");
-                                    
-                                    decimal valorConv = decimal.Parse(valor);
-                                    valorConv = valorConv * (-1);
-                                    Model.Matriz[i, j] = valorConv.ToString();
-                                }
-                                else 
-                                {
-                                    Model.Matriz[i, j] = "0";
-                                }
                             }
                         }
                     }
                 }
-            }
 
-            if(MatrixA == null)
-            {
-                return View(Model);
-            }
-            else
-            {
-                Model.Titulo = "Tabela Inicial";
-                int contador = 1;
-                ListaTabelas.Add(Model);
-
-                //HeaderSimplex ModelTopZera = new HeaderSimplex();
-                //ModelTopZera = Model;
-                string[,] Tabela = Model.Matriz;
-                while (!CondicaoDeParada(Tabela))
+                if (MatrixA == null)
                 {
-                    Tabela = SolveSimplex(Tabela);
-
-                    HeaderSimplex NovoModelo = new HeaderSimplex();
-                    NovoModelo.Matriz = Tabela;
-                    NovoModelo.Titulo = contador + "ª Iteração";
-                    contador++;
-
-                    ListaTabelas.Add(NovoModelo);
+                    return View(Model);
                 }
+                else
+                {
+                    Model.Titulo = "Tabela Inicial";
+                    int contador = 1;
+                    ListaTabelas.Add(Model);
 
-                return View("Resultado", ListaTabelas);
+                    //HeaderSimplex ModelTopZera = new HeaderSimplex();
+                    //ModelTopZera = Model;
+                    string[,] Tabela = Model.Matriz;
+
+                    while (!CondicaoDeParada(Tabela) & contador <= 20)
+                    {
+                        Tabela = SolveSimplex(Tabela);
+
+                        HeaderSimplex NovoModelo = new HeaderSimplex();
+                        NovoModelo.Matriz = Tabela;
+                        NovoModelo.Titulo = contador + "ª Iteração";
+
+                        //DEFINIR LIMITE DE ITERAÇÕES
+                        contador++;
+
+                        ListaTabelas.Add(NovoModelo);
+                    }
+
+                    if(contador > 20)
+                    {
+                        throw new Exception("Limite de iterações excedido!");
+                    }
+
+                    return View("Resultado", ListaTabelas);
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewData["Exception"] = ex.Message;
+                return View("Erro");
             }
         }
 
@@ -147,41 +163,53 @@ namespace PerSymplex.Controllers
             string[,] NovaTabela = new string[Tabela.GetLength(0), Tabela.GetLength(1)];
             
             PCol = findPCol(Tabela);
-            PLin = findPLin(Tabela, PCol);
-            //basis[PLin] = PCol;
 
-            for (int i = 0; i < Tabela.GetLength(0); i++)
-                NovaTabela[i, 0] = Tabela[i, 0];
-
-            for (int j = 0; j < Tabela.GetLength(1); j++)
-                NovaTabela[0, j] = Tabela[0,j];
-
-            NovaTabela[PLin, 0] = Tabela[0, PCol];
-
-            for (int j = 1; j < Tabela.GetLength(1); j++)
-                NovaTabela[PLin, j] = (decimal.Parse(Tabela[PLin, j]) / decimal.Parse(Tabela[PLin, PCol])).ToString();
-
+            bool SolLimitda = false;
             for (int i = 1; i < Tabela.GetLength(0); i++)
+                if (decimal.Parse(Tabela[i, PCol]) > 0)
+                    SolLimitda = true;
+
+            if (SolLimitda)
             {
-                if (i == PLin)
-                    continue;
+                PLin = findPLin(Tabela, PCol);
+
+                for (int i = 0; i < Tabela.GetLength(0); i++)
+                    NovaTabela[i, 0] = Tabela[i, 0];
+
+                for (int j = 0; j < Tabela.GetLength(1); j++)
+                    NovaTabela[0, j] = Tabela[0, j];
+
+                NovaTabela[PLin, 0] = Tabela[0, PCol];
 
                 for (int j = 1; j < Tabela.GetLength(1); j++)
-                    NovaTabela[i, j] = (decimal.Parse(NovaTabela[PLin, j]) * (decimal.Parse(Tabela[i, PCol]) * (-1)) + decimal.Parse(Tabela[i,j])).ToString();
-                //NovaTabela[i, j] = (decimal.Parse(Tabela[i, j]) - (decimal.Parse(Tabela[i, PCol]) * decimal.Parse(NovaTabela[PLin, j]))).ToString();
-            }
-            //table = NovaTabela;
+                    NovaTabela[PLin, j] = (decimal.Parse(Tabela[PLin, j]) / decimal.Parse(Tabela[PLin, PCol])).ToString();
 
-            // Count a result found by the X
-            //for (int i = 0; i < result.Length; i++)
-            //{
-            //    int k = basis.IndexOf(i + 1);
-            //    if (k != -1)
-            //        result[i] = table[k, 0];
-            //    else
-            //        result[i] = 0;
-            //}
-            return NovaTabela;
+                for (int i = 1; i < Tabela.GetLength(0); i++)
+                {
+                    if (i == PLin)
+                        continue;
+
+                    for (int j = 1; j < Tabela.GetLength(1); j++)
+                        NovaTabela[i, j] = (decimal.Parse(NovaTabela[PLin, j]) * (decimal.Parse(Tabela[i, PCol]) * (-1)) + decimal.Parse(Tabela[i, j])).ToString();
+                    //NovaTabela[i, j] = (decimal.Parse(Tabela[i, j]) - (decimal.Parse(Tabela[i, PCol]) * decimal.Parse(NovaTabela[PLin, j]))).ToString();
+                }
+                //table = NovaTabela;
+
+                // Count a result found by the X
+                //for (int i = 0; i < result.Length; i++)
+                //{
+                //    int k = basis.IndexOf(i + 1);
+                //    if (k != -1)
+                //        result[i] = table[k, 0];
+                //    else
+                //        result[i] = 0;
+                //}
+                return NovaTabela;
+            }
+            else
+            {
+                throw new Exception("Não foi possível resolver pois a solução é ilimitada!");
+            }
         }
 
         public bool CondicaoDeParada(string[,] Tabela)
